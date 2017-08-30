b240bee8a5109e594e7eaf5d0768db67002f56b9
Sergey Grigorev <kitetsy@gmail.com>
2017-08-30 21:25:56 +0300

    fix example

--- example/source/app.d
+++ example/source/app.d
@@ -37,12 +37,13 @@ void main()
     writeln;
     foreach (v; db.qSelect!Bar.where("value <", 3).run)
         writeln(v);
+
+    db.qDelete!Foo.where("ts <", cts - cast(ulong)1e8).run;
     
-    db.qInsert(Foo(0, "hello", cts), Foo(0, "world", cts));
+    db.qInsert(Foo(0, "hello", cts), Foo(20, "world", cts));
+    db.qInsert(Foo(9210, "hello", cts), Foo(0, "world", cts));
     import std.random : uniform;
     db.qInsert(Bar(0, uniform(0, 10)));
 
-    db.qInsertOrReplace(Foo(0, "hello", cts), Foo(1, "world", cts));
-
-    db.qDelete!Foo.where("ts <", cts - cast(ulong)1e8).run;
+    db.qInsertOrReplace(Foo(1, "hello", cts), Foo(3, "world", cts));
 }
